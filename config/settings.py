97067c266e2b942475c67b9038c528204934aa80
Murodjon Shermuhamedov <100022047+murodjon001@users.noreply.github.com>
2023-11-20 17:58:36 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Mrit',
         'USER': 'postgres',
-        'PASSWORD': '06102000murodjon',
+        'PASSWORD': '*********',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
